2846ec684f872bbabac4e97f5063f0c1991f565a
iqrausama97 <iqra10412@gmail.com>
2024-03-27 02:33:30 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,18 @@
+import inquirer from "inquirer";
+
+
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Enter your guess number:"
+}
+])
+
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("Congratulations! you guessed a right number.");
+}else {
+    console.log("You guessed a wrong number.");
+}
